b684a615113ca76cc47801664f6142f54104d8a1
AndrewMatheny <github email address>
2020-02-04 15:15:03 -0800

    filtering purchases by date maybe

--- src/containers/BudgetBar.js
+++ src/containers/BudgetBar.js
@@ -1,12 +1,40 @@
 import React from 'react'
 
+let currentDate = new Date()
+let month = currentDate.getMonth() + 1
+if(month < 10) {
+    month = `0${month}`
+}
+let day = currentDate.getDate()
+if(day < 10) {
+    day = `0${day}`
+}
+
+let formatDate = `${month}/${day}/${currentDate.getFullYear()}`
+
 export default class BudgetBar extends React.Component {
 
     calculateSpent = () => {
-        let allTransactions = this.props.budget.transactions 
-        console.log(allTransactions)
+        let allTransactions = this.props.budget.transactions
+        // console.log(formatDate)
+        let filterTransactions = []
+        // console.log(allTransactions)
         return allTransactions.map(transaction => {
-            return console.log(new Date(transaction.date))
+            let transactionDate = new Date(transaction.date)
+            let tday = transactionDate.getDate()
+            if(tday < 10) {
+                tday = `0${tday}`
+            }
+            let tmonth = transactionDate.getMonth() + 1
+            if(tmonth < 10) {
+                tmonth = `0${tmonth}`
+            }
+            let formatTransactionDate = `${tmonth}/${tday}/${transactionDate.getFullYear()}`
+            console.log(formatTransactionDate)
+            console.log(formatDate)
+            if(formatTransactionDate === formatDate) {
+                return transaction
+            }
         })
     }
 
