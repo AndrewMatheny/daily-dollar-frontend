deaf70ed863c5550c5820ae1516358f9b4ac6051
AndrewMatheny <github email address>
2020-03-03 17:00:36 -0800

    bar changes color as budget is used up

--- src/containers/CustomBar.js
+++ src/containers/CustomBar.js
@@ -4,13 +4,39 @@ import { Progress, Segment } from 'semantic-ui-react'
 
 class CustomBar extends React.Component {
 
-    render() {
-        return (
-            <div style={{display: 'inline-block', width: "80%"}}>
+    renderBar = () => {
+        let value = this.props.value
+        let total = this.props.total
+        if(value/total > 0.8) {
+            return (
+                <div style={{display: 'inline-block', width: "80%"}}>
+                <Segment inverted>
+                            <Progress inverted value={this.props.value} total={this.props.total} progress='ratio' color="red" size="large"/>
+                    </Segment>
+            </div>
+            )
+        } else if(value/total <= 0.5) {
+            return (
+                <div style={{display: 'inline-block', width: "80%"}}>
                 <Segment inverted>
                             <Progress inverted value={this.props.value} total={this.props.total} progress='ratio' color="green" size="large"/>
                     </Segment>
             </div>
+            )
+        } else {
+            return (
+                <div style={{display: 'inline-block', width: "80%"}}>
+                <Segment inverted>
+                            <Progress inverted value={this.props.value} total={this.props.total} progress='ratio' color="yellow" size="large"/>
+                    </Segment>
+            </div>
+            )
+        }
+    }
+
+    render() {
+        return (
+            this.renderBar()
             );
         };
 }
