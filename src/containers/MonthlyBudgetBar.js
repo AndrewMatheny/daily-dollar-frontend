929b9136a5ba5f34e1e7c87f57650de161c2daf4
AndrewMatheny <github email address>
2020-03-04 09:36:26 -0800

    rewrote monthly budget bar to allow cleaner deletes and fixed bugs - need to do daily next

--- src/containers/MonthlyBudgetBar.js
+++ src/containers/MonthlyBudgetBar.js
@@ -8,21 +8,16 @@ export default class MonthlyBudgetBar extends React.Component {
 
     state = {
         showTransactions: false,
-        monthlyTransactions: ""
+        // monthlyTransactions: ""
     }
 
-    componentDidMount() {
-        this.setMonthlyTransactions()
-    }
-
-    componentWillReceiveProps() {
-        this.setMonthlyTransactions()
-    }
-
-    // static getDerivedStateFromProps(props, state) {
+    // componentDidMount() {
     //     this.setMonthlyTransactions()
     // }
 
+    // componentWillReceiveProps() {
+    //     this.setMonthlyTransactions()
+    // }
 
     handleClick = () => {
         this.setState(prevState => ({
@@ -30,26 +25,28 @@ export default class MonthlyBudgetBar extends React.Component {
         }))
     }
 
-    setMonthlyTransactions = () => {
-        let monthlyT = this.matchTransactionDate()
-        this.setState({
-            monthlyTransactions: monthlyT
-        })
-    }
+    // setMonthlyTransactions = () => {
+    //     let monthlyT = this.matchTransactionDate()
+    //     this.setState({
+    //         monthlyTransactions: monthlyT
+    //     })
+    // }
 
-    filterMonthlyTransactions = transactionObj => {
-        this.props.deleteTransaction(transactionObj)
-        let newMonthly = this.state.monthlyTransactions.filter(transaction => transaction !== transactionObj)
-        this.setState({
-            monthlyTransactions: newMonthly
-        })
-    }
+    // filterMonthlyTransactions = transactionObj => {
+    //     this.props.deleteTransaction(transactionObj)
+        // this.showTransactions()
+        // this.setMonthlyTransactions()
+        // let newMonthly = this.state.monthlyTransactions.filter(transaction => transaction !== transactionObj)
+        // this.setState({
+        //     monthlyTransactions: newMonthly
+        // })
+    // }
 
     showTransactions = () => {
         if(this.state.showTransactions) {
-            let monthlyBudget = { transactions: this.state.monthlyTransactions}
+            let monthlyBudget = { transactions: this.matchTransactionDate()}
             return (
-                <TransactionTable budget={monthlyBudget} deleteTransaction={this.filterMonthlyTransactions}/>
+                <TransactionTable budget={monthlyBudget} deleteTransaction={this.props.deleteTransaction}/>
             )
         }
     }
@@ -105,9 +102,6 @@ export default class MonthlyBudgetBar extends React.Component {
                 <br></br>
                 <h1>{this.props.budget.name} Monthly Budget</h1>
                 <h1>{spend} / {this.props.budget.limit}</h1>
-                {/* <Segment inverted>
-                    <Progress inverted value={spend} total={this.props.budget.limit} progress='ratio' color="green"/>
-                </Segment> */}
                 <CustomBar value={spend} total={this.props.budget.limit}/>
                 <br></br>
                 <Button inverted color="red" onClick={() => this.props.deleteBudget(this.props.budget)}>Delete Budget</Button>
