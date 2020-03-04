e6709c2d7cbe10338540138dec63bfd184501674
AndrewMatheny <github email address>
2020-03-04 15:13:43 -0800

    placeholder homepage with footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,86 @@
 import React from "react"
 // import { Link } from 'react-router-dom'
 import logo from '../images/DailyDollarNoBack.png';
+import { Container, Grid, Header, Divider, Icon, Image, List, Menu, Responsive, Segment, Sidebar, Visibility, Dropdown } from "semantic-ui-react"
 
-const Home = props => {
 
-  return (
-      <div style={{textAlign: 'center', color: "white"}}>
-   
+const Home = () => (
+    <div>
+  
+      <Container text style={{ marginTop: '7em', color:"white", textAlign: "center" }}>
+        {/* <Header as='h1'>Semantic UI React Fixed Template</Header> */}
+        
+        <div style={{textAlign: 'center', color: "white"}}>
+            <img className="mainLogo" src={logo} alt="Daily Dollar Logo"></img>
+        </div>
 
+        <h1>Welcome to Daily Dollar</h1>
         <br></br>
-        <br></br>
-        <img className="mainLogo" src={logo} alt="Daily Dollar Logo"></img>
-        <br></br>
-        <h1>Place home info here</h1>
-      </div>
-  );
-};
+        <p>This is a basic fixed menu template using fixed size containers.</p>
+        <p>
+          A text container is used for the main container, which is useful for single column layouts.
+        </p>
+  
+      </Container>
+  
+      <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+        <Container textAlign='center'>
+          <Grid divided inverted stackable>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Group 1' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+                <List.Item as='a'>Link Two</List.Item>
+                <List.Item as='a'>Link Three</List.Item>
+                <List.Item as='a'>Link Four</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Group 2' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+                <List.Item as='a'>Link Two</List.Item>
+                <List.Item as='a'>Link Three</List.Item>
+                <List.Item as='a'>Link Four</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Group 3' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+                <List.Item as='a'>Link Two</List.Item>
+                <List.Item as='a'>Link Three</List.Item>
+                <List.Item as='a'>Link Four</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header inverted as='h4' content='Footer Header' />
+              <p>
+                Extra space for a call to action inside the footer that could help re-engage users.
+              </p>
+            </Grid.Column>
+          </Grid>
+  
+          <Divider inverted section />
+          <Image centered size='mini' src='/logo.png' />
+          <List horizontal inverted divided link size='small'>
+            <List.Item as='a' href='#'>
+              Site Map
+            </List.Item>
+            <List.Item as='a' href='#'>
+              Contact Us
+            </List.Item>
+            <List.Item as='a' href='#'>
+              Terms and Conditions
+            </List.Item>
+            <List.Item as='a' href='#'>
+              Privacy Policy
+            </List.Item>
+          </List>
+        </Container>
+      </Segment>
+    </div>
+  )
+  
 
 export default Home
